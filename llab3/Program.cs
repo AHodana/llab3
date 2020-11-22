b31de8e71aa8c6afa62928d8780bac9d76ff6adc
hodana66 gmail.com <hodana66@gmail.com>
2020-11-22 14:54:36 +0100

    2nd commit

--- llab3/Program.cs
+++ llab3/Program.cs
@@ -6,7 +6,7 @@ namespace llab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Art");
         }
     }
 }
